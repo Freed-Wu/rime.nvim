43151d9222a49a5bb9bc5af47d03b8d7f807af0e
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-07-10 08:04:02 +0800

    :bulb: Add comment for shell.nix
    Fix typo of shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
+# luarocks --local install rime.nvim-scm-1.rockspec RIME_INCDIR=$(pkg-config --variable=includedir rime) RIME_LIBDIR=$(pkg-config --variable=libdir rime)
 { pkgs ? import <nixpkgs> { } }:
 
 with pkgs;
 mkShell {
   name = "rime.nvim";
   buildInputs = [
-    rime.dev
+    pkg-config
+    librime
   ];
 }
