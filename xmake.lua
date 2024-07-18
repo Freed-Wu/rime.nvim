f1ec341a8ae662e748f4c21954788fbf700e53c9
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-07-18 22:51:05 +0800

    :bug: Support Android Termux and ArchLinux's lua header paths

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,10 @@
 -- https://github.com/luarocks/luarocks/discussions/1695
 add_rules("mode.debug", "mode.release")
 
+-- Android Termux and ArchLinux's lua are not neovim's lua
+local prefix = os.getenv("PREFIX") or "/usr"
+add_includedirs(prefix .. "/include/luajit-2.1")
+add_includedirs(prefix .. "/include/lua5.1")
 add_requires("rime")
 
 target("rime")
