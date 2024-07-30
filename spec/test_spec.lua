d499ca83b896e8a088a07159fc2eb6f45c8e6710
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-07-30 14:02:17 +0800

    :white_check_mark: Add unit tests

---
+++ spec/test_spec.lua
@@ -0,0 +1,10 @@
+local parse_key = require "lua.nvim-rime.parse_key"
+
+-- luacheck: ignore 113
+---@diagnostic disable: undefined-global
+describe("test", function()
+    it("tests parse_key", function()
+        assert.are.equal(parse_key("<C-S>", {}), parse_key("s", { "Control" }))
+        assert.are.equal(parse_key("<M-C-S>", {}), parse_key("s", { "Control", "Alt" }))
+    end)
+end)
