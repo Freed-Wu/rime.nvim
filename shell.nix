db9fa1001a96bfb17b200b537cdc6e173a928f14
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-12-08 18:15:53 +0800

    :heavy_plus_sign: Add busted to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,22 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 with pkgs;
 mkShell {
   name = "rime.nvim";
   buildInputs = [
-    pkg-config
     librime
-    luajit
-    xmake
+
     stdenv.cc
+    pkg-config
+    xmake
+
+    (luajit.withPackages (
+      p: with p; [
+        busted
+      ]
+    ))
   ];
   # https://github.com/NixOS/nixpkgs/issues/314313#issuecomment-2134252094
   shellHook = ''
