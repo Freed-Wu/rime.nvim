beaa97e8bc80701e1381717739158a0a8e221759
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-08-12 23:50:48 +0800

    :hammer: Update scripts/update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,4 @@
 set -e
 cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-nix-shell --run 'luarocks install --lua-version 5.1 --force --local rime.nvim'
+nix-shell --run 'luarocks install --force rime.nvim'
